ee4c444bfd051e594fc0d18e9229368cd9ea42d7
harshabakku <bakkubaku@gmail.com>
2020-12-04 18:11:45 +0530

    npm init, add relayer js code, cli command code to start relayer

---
+++ cli/start/eth2hmy-relay.js
@@ -0,0 +1,40 @@
+const ProcessManager = require('pm2')
+const { spawnProcess } = require('./helpers')
+const { Eth2HmyRelay } = require('../../lib/eth2hmy-relay')
+const { BridgeConfig } = require('harmony-bridge-lib/config')
+const path = require('path')
+const os = require('os')
+
+class StartEth2HmyRelayCommand {
+  static async execute() {
+    if (BridgeConfig.getParam('daemon') === 'true') {
+      ProcessManager.connect((err) => {
+        if (err) {
+          console.log(
+            'Unable to connect to the ProcessManager daemon! Please retry.'
+          )
+          return
+        }
+        spawnProcess('eth2Hmy-relay', {
+          name: 'eth2Hmy-relay',
+          script: path.join(__dirname, '../../index.js'),
+          interpreter: 'node',
+          error_file: '~/.harmonybridge/logs/eth2hmy-relay/err.log',
+          out_file: '~/.harmonybridge/logs/eth2hmy-relay/out.log',
+          args: ['start', 'eth2hmy-relay', ...BridgeConfig.getArgsNoDaemon()],
+          wait_ready: true,
+          kill_timeout: 60000,
+          logDateFormat: 'YYYY-MM-DD HH:mm:ss.SSS',
+        })
+      })
+    } else {
+      
+      const relay = new Eth2HmyRelay()
+      relay.initialize()
+      console.log('Starting eth2hmy-relay...')
+      await relay.run()
+    }
+  }
+}
+
+exports.StartEth2HmyRelayCommand = StartEth2HmyRelayCommand

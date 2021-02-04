826b19290bd99dc34c040db3f58b1c9072a976a9
peekpi <894646171@qq.com>
2021-02-04 23:13:23 +0800

    fixed a bug of merkel tree generate

--- elc/ethash/contracts/ethash/MerkelRoot.sol
+++ elc/ethash/contracts/ethash/MerkelRoot.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.6.2;
 
 contract MerkelRoots {
-    uint64 constant epochStart = 386;
-    uint64 constant epochEnd = 386;
-    bytes constant ROOTS = "\xaa\x35\xd6\xe6\x49\xed\xf7\x9b\x9e\x83\xe1\xc8\xf1\xf8\x77\x8a\x5c\x18\xba\x28\x06\x6c\x88\x59\x03\x97\x44\xac\x01\x5c\x0a\xda";
+    uint64 constant epochStart = 387;
+    uint64 constant epochEnd = 387;
+    bytes constant ROOTS = "\x58\xe6\xb0\x93\x46\x3d\x54\x6a\xa9\x8c\x47\x30\xe2\x29\x04\xd1\x61\x93\xf3\xdd\x9d\xf3\x6c\x46\x8c\x27\xa5\x8c\xb9\xb5\xa8\x6e";
     
    function getRootHash(uint64 epoch) internal pure returns(bytes32 hash) {
        bytes memory roots = ROOTS;

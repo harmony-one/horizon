65c6c819bd8ead06d2942778712f1eb87300d40c
peekpi <894646171@qq.com>
2021-03-08 22:46:33 +0800

    switch to the old bridge contract

--- bridge/contracts/LightClient.sol
+++ bridge/contracts/LightClient.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.6.2;
 
-import {Ownable} from "openzeppelin-solidity/contracts/access/Ownable.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
 interface ILightClient {
     function VerifyReceiptsHash(bytes32 blockHash, bytes32 receiptsHash) external returns(bool);
@@ -14,6 +14,7 @@ contract LightClientUnsafe is ILightClient {
 }
 
 contract LightClientTrust is ILightClient,Ownable {
+    constructor() Ownable() public {}
     mapping(bytes32=>bytes32) public Receipts;
     function VerifyReceiptsHash(bytes32 blockHash, bytes32 receiptsHash) override public returns(bool) {
         return Receipts[blockHash] == receiptsHash;

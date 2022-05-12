6dd55f8a7716b7603ed6245d0afbc95074414003
DakBuk <brucdarc@gmail.com>
2022-05-12 14:49:24 -0600

    fix bug where canonical head was not being set on fork rewrite

--- contracts/EthereumLightClient.sol
+++ contracts/EthereumLightClient.sol
@@ -210,6 +210,8 @@ contract EthereumLightClient is Ethash, Initializable, PausableUpgradeable {
             current = blocks[current].parentHash;
         }
 
+        canonicalHead = _blockHash;
+
     }
 
     function isVerified(uint256 blockHash)

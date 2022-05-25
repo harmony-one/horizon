28c9097fceda21e863ba9bbc69e4fc5e0cdd3c7b
DakBuk <brucdarc@gmail.com>
2022-05-25 06:50:47 -0600

    fix var to let in bridge.hmy.js

--- test/bridge.hmy.js
+++ test/bridge.hmy.js
@@ -9,7 +9,7 @@ let MMRVerifier, HarmonyProver;
 let prover, mmrVerifier;
 
 function hexToBytes(hex) {
-    for (var bytes = [], c = 0; c < hex.length; c += 2)
+    for (let bytes = [], c = 0; c < hex.length; c += 2)
         bytes.push(parseInt(hex.substr(c, 2), 16));
     return bytes;
 }
